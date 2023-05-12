264c33c9d4a1c1908ce812e8fe074ded63cac3a1
Tegar <tegardanardana991@gmail.com>
2023-05-12 21:01:03 +0700

    create table users

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize(
-    'main-db',
-    'root',
-    '',
-     {
-       host: 'localhost',
-       dialect: 'mysql'
-     }
-)
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
- }).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
- });
-
+const {users} = require('./models')
 
 app.get('/', (req, res) => {
   res.send('Welcome to Growthy')
 })
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/users', async(req, res) => {
+  await users.findAll().then(users => {
+  res.send(users)  }).catch(error=> 
+    console.log(error))
 })
---
+++ seeders/20230512134127-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [{
+      name: 'John',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Gito',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

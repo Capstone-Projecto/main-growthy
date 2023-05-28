055148a97880c1dcb011c4f8e4cdbf8d741995d3
Tegar <tegardanardana991@gmail.com>
2023-05-28 20:19:39 +0700

    feat: remove autoMl configuration

--- route/disease.route.js
+++ route/disease.route.js
@@ -1,64 +1,6 @@
 require('dotenv').config();
 const { disease, plants, plants_has_disease } = require('../models');
 const route = require("express").Router();
-// const { Storage } = require('@google-cloud/storage');
-// const { PredictionServiceClient } = require('@google-cloud/automl').v1;
-
-// // Membuat instance Google Cloud Storage
-// const storage = new Storage();
-// // Membaca konfigurasi GCP dari file .env
-// const projectId = process.env.GCP_PROJECT_ID;
-// const bucketName = process.env.DISEASE_ML_BUCKET;
-
-// // Membuat instance Google Cloud AutoML Prediction Service
-// const predictionServiceClient = new PredictionServiceClient();
-// // Membaca konfigurasi GCP dari file .env
-// const modelId = process.env.GCP_AUTOML_MODEL_ID;
-
-// // Endpoint untuk mendeteksi penyakit berdasarkan gambar
-// route.post('/diseases/detect', async (req, res) => {
-//   try {
-//     // Mendapatkan file gambar dari body request
-//     const imageFile = req.files.image;
-    
-//     // Upload file gambar ke Google Cloud Storage
-//     const bucket = storage.bucket(bucketName);
-//     const gcsFileName = `${Date.now()}_${imageFile.name}`;
-//     const file = bucket.file(gcsFileName);
-//     await file.save(imageFile.data);
-//     await file.makePublic();
-
-//     // Mendapatkan URL publik file gambar yang diunggah
-//     const imageUrl = `https://storage.googleapis.com/${bucketName}/${gcsFileName}`;
-
-//     // Memanggil model machine learning untuk melakukan prediksi
-//     const request = {
-//       name: predictionServiceClient.modelPath(projectId, 'asia-southeast2', modelId),
-//       payload: {
-//         image: {
-//           imageBytes: imageFile.data.toString('base64')
-//         }
-//       }
-//     };
-//     const [response] = await predictionServiceClient.predict(request);
-
-//     // Mendapatkan hasil prediksi
-//     const predictions = response.payload.map((prediction) => {
-//       return {
-//         displayName: prediction.displayName,
-//         score: prediction.classification.score
-//       };
-//     });
-
-//     res.send({
-//       imageUrl,
-//       predictions
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 // Mendapatkan semua penyakit tanaman beserta tanaman terkait
 route.get('/diseases', async (req, res) => {
--- route/plants.route.js
+++ route/plants.route.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
 const { disease, plants, plants_has_disease } = require('../models');
 const route = require("express").Router();
-// const { Storage } = require('@google-cloud/storage');
-// const { PredictionServiceClient } = require('@google-cloud/automl').v1;
-
-// // Konfigurasi Google Cloud Storage
-// const storage = new Storage({
-//   projectId: process.env.GCP_PROJECT_ID,
-// });
-
-// // Konfigurasi Google Cloud AutoML
-// const predictionClient = new PredictionServiceClient();
-// const modelFullId = predictionClient.modelPath(
-//   process.env.GCP_PROJECT_ID,
-//   'asia-southeast2',
-//   process.env.GCP_AUTOML_MODEL_ID
-// );
 
 // Mengambil semua tanaman beserta penyakit terkait
 route.get('/plants', async (req, res) => {
@@ -69,74 +54,5 @@ route.get('/plants/:id', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
-// // Mendeteksi tanaman berdasarkan gambar yang diunggah oleh pengguna
-// route.post('/plants/detect', async (req, res) => {
-//   try {
-//     // Mengambil file gambar yang diunggah dari body request
-//     const file = req.files.image;
-
-//     // Menyimpan file gambar ke Google Cloud Storage
-//     const bucketName = process.env.PLANTS_ML_BUCKET;
-//     const filePath = `${Date.now()}_${file.name}`;
-//     const fileOptions = {
-//       destination: filePath,
-//       public: true
-//     };
-//     await storage.bucket(bucketName).upload(file.data, fileOptions);
-
-//     // Mendapatkan URL publik file yang telah diunggah
-//     const fileUrl = `https://storage.googleapis.com/${bucketName}/${filePath}`;
-
-//     // Mengirim permintaan prediksi ke model AutoML
-//     const payload = {
-//       image: {
-//         imageBytes: file.data
-//       }
-//     };
-//     const [response] = await predictionClient.predict({
-//       name: modelFullId,
-//       payload: payload
-//     });
-
-//     // Mendapatkan hasil prediksi dari respons AutoML
-//     const predictions = response.payload.map((prediction) => ({
-//       label: prediction.displayName,
-//       score: prediction.classification.score
-//     }));
-
-//     // Mencari tanaman berdasarkan label prediksi
-//     const detectedPlant = await plants.findOne({
-//       include: [
-//         {
-//           model: disease,
-//           as: 'disease',
-//           through: {
-//             model: plants_has_disease,
-//             attributes: []
-//           }
-//         }
-//       ],
-//       where: {
-//         plant_desc: predictions[0].label
-//       }
-//     });
-
-//     if (detectedPlant) {
-//       res.send({
-//         detectedPlant,
-//         predictions
-//       });
-//     } else {
-//       res.send({
-//         message: 'Tanaman tidak ditemukan',
-//         predictions
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 module.exports = route;

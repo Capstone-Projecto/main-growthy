8403c3173353bdc82b0cc03b855ab376a94a0e63
Tegar <tegardanardana991@gmail.com>
2023-06-02 22:41:31 +0700

    feat: optimized auth.route jwt

--- route/auth.route.js
+++ route/auth.route.js
@@ -1,38 +1,37 @@
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 const { users } = require('../models');
-const route = require("express").Router();
+const route = require('express').Router();
 const jwt = require('jsonwebtoken');
-require('../config.js');
 const passport = require('passport');
+require('../config.js');
 
 // Endpoint for Google authentication
-route.get('/google', passport.authenticate('google', {
-  scope: ['profile', 'email'],
-  successRedirect: '/auth/google/protected',
-  failureRedirect: '/auth/google/failure'
-}));
+route.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 // Endpoint called after successful Google authentication
-route.get('/google/protected', async (req, res) => {
-  const name = req.user.displayName;
-  const email = req.user.email;
-  const avatar = req.user.picture;
+route.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/auth/google/failure' }),
+  async (req, res) => {
+    const { id, displayName: name, email, picture: avatar } = req.user;
 
-  await users.findOrCreate({
-    where: { name: name, email: email, avatar: avatar }
-  });
-  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET);
-  return res.redirect(`/auth/google/success/${token}`);
-});
+    try {
+      const [user, created] = await users.findOrCreate({
+        where: { id },
+        defaults: { name, email, avatar }
+      });
 
-// Endpoint called after successful Google authentication
-route.get('/google/success/:token', (req, res) => {
-  res.send({
-    message: 'Login success',
-    token: req.params.token
-  });
-});
+      const token = generateAuthToken(user);
+      res.redirect(`/auth/google/success/${token}`);
+    } catch (error) {
+      console.error(error);
+      res.redirect('/auth/google/failure');
+    }
+  }
+);
 
 // Endpoint called when Google authentication fails
 route.get('/google/failure', (req, res) => {
@@ -41,34 +40,29 @@ route.get('/google/failure', (req, res) => {
 
 // Test endpoint for authentication with Google
 route.get('/test', (req, res) => {
-  res.send(
-    '<a href="/auth/google">Authenticate with Google</a>'
-  );
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
 // Login logic
 route.post('/login', async (req, res) => {
   try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const isUserExist = await users.findOne({ where: { email: email } });
+    const { email, password } = req.body;
+
+    const user = await users.findOne({ where: { email } });
 
     // Check if user exists
-    if (!isUserExist) {
+    if (!user) {
       return res.status(404).json({ message: 'User not found' });
-    } else {
-      const isPasswordMatch = bcrypt.compareSync(password, isUserExist.password);
-      const token = jwt.sign({ id: isUserExist.id }, process.env.JWT_SECRET);
-
-      if (isPasswordMatch) {
-        return res.json({
-          message: 'Login success',
-          token: token
-        });
-      } else {
-        return res.status(401).json({ message: 'Wrong password' });
-      }
     }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: 'Wrong password' });
+    }
+
+    const token = generateAuthToken(user);
+    return res.json({ message: 'Login success', token });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
@@ -78,26 +72,37 @@ route.post('/login', async (req, res) => {
 // Register logic
 route.post('/register', async (req, res) => {
   try {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
+    const { name, email, password } = req.body;
 
-    // Hash password
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(password, salt);
+    const userExists = await users.findOne({ where: { email } });
 
-    // Check if user already exists
-    const isUserExist = await users.findOne({ where: { email: email } });
-    if (isUserExist) {
+    if (userExists) {
       return res.status(409).json({ message: 'User already exists' });
-    } else {
-      await users.create({ name: name, email: email, password: hash });
-      return res.json({ message: 'Register success' });
     }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await users.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    const token = generateAuthToken(user);
+    return res.json({ message: 'Register success', token });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
+// Helper function to generate JWT token
+const generateAuthToken = (user) => {
+  const { id, name, email } = user;
+  return jwt.sign({ id, name, email }, process.env.JWT_SECRET, {
+    expiresIn: '1d'
+  });
+};
 
 module.exports = route;
